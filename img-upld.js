6d2571476580d8cf15f49de8eeafc01ff477c75b
ItayFrisk <itay.manevych@gmail.com>
2022-12-08 20:36:21 +0200

    added the option to upload images, including file format validation, but the styling is not finished

--- img-upld.js
+++ img-upld.js
@@ -2,13 +2,19 @@
 const realFileBtn = document.getElementById("real-file");
 const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
+const regexPattern = ("(.*?)\.(jpg|gif|doc|pdf)$");
+
 
 customBtn.addEventListener("click", ()=>{
     realFileBtn.click();
-  })
+})
 
 realFileBtn.addEventListener("change", ()=>{
     if(realFileBtn.value){
+        if((realFileBtn.value.match(/(.*?)\.(jpg|gif|doc|pdf)$/) == null))
+        {
+            customTxt.innerHTML = "File format not supported"
+        }
         customTxt.innerHTML= realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
     }
     else{
