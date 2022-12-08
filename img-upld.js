f9217230be169895f502c15e1df7eb032745e0ae
ItayFrisk <itay.manevych@gmail.com>
2022-12-08 21:34:01 +0200

    fixed the regex and the way i keep the name of the file without the path and added comments

--- img-upld.js
+++ img-upld.js
@@ -2,21 +2,28 @@
 const realFileBtn = document.getElementById("real-file");
 const customBtn = document.getElementById("custom-button");
 const customTxt = document.getElementById("custom-text");
-const regexPattern = ("(.*?)\.(jpg|gif|doc|pdf)$");
+const regexPattern = /([a-zA-Z0-9\s_\\.\-\(\):])+(png|jpg|jpeg|gif)$/i;
 
-
+// open the file explorer when the custom button is clicked
 customBtn.addEventListener("click", ()=>{
     realFileBtn.click();
 })
 
+// change the custom text 
 realFileBtn.addEventListener("change", ()=>{
     if(realFileBtn.value){
-        if((realFileBtn.value.match(/(.*?)\.(jpg|gif|doc|pdf)$/) == null))
+        // using regex the check if the file format is supported or not
+        if(!(realFileBtn.value.match(regexPattern)))
         {
-            customTxt.innerHTML = "File format not supported"
+            customTxt.innerHTML = "File format isn't supported"
         }
-        customTxt.innerHTML= realFileBtn.value.match(/[\/\\]([\w\d\s\.\-\(\)]+)$/)[1];
+
+        else{
+            customTxt.innerHTML = realFileBtn.value.replace('C:\\fakepath\\', '');
+        }
     }
+
+    // if there was no photo uploaded then keep the custom text the same
     else{
         customTxt.innerHTML = "Upload Image";
     }
